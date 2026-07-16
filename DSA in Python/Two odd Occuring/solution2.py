57a9827934bdf573a83811af87fab5133df53317
SHIVAM SINGH <146062311+shivtheuser@users.noreply.github.com>
2026-07-16 11:02:19 +0530

    Create solution2.py
    find the rightmost set bit of ans by while loop but S(c)= 1

---
+++ DSA in Python/Two odd Occuring/solution2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def twoOddNum(self, arr):
+        #code here 
+        #find the xor
+        ans = 0
+        for i in arr:
+            ans ^= i
+        
+        #find the set bit in ans
+        set_bit = 0
+        while ans & (1<<set_bit) == 0:
+            set_bit += 1
+            
+        # setbit element in ans_set and visa vera
+         #find the two odd pairs
+        xor1 = 0
+        xor2 = 0
+        
+        for i in arr:
+            if i&(1<<set_bit):
+                xor1 ^= i
+            else:
+                xor2 ^= i
+        
+        return sorted([xor1, xor2], reverse=True)
+                
+       
+        
+        
